31e1dcad548981a924dccdb9d16e40601d5587f5
burakaktan <61063293+burakaktan@users.noreply.github.com>
2021-03-14 17:31:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@ using namespace std;
 
 int main()
 {
-    myArrayList<int> *al = new myArrayList<int>();
+    myArrayList<int> *a1 = new myArrayList<int>();
+    myArrayList<int> *a2 = new myArrayList<int>();
+    myArrayList<int> *a3 = new myArrayList<int>();
     for(int i = 0;i<10;i++)
     {
-        al->add(3*i+1);
+        a1->add(3*i+1);
+        a2->add(3*i+1);
+        a3->add(3*i-1);
     }
-    al->print();
+    a1->print();
+
+    cout<<"Is a1 same as a2: "<< (*a1 == *a2) <<endl;
+    cout<<"Is a1 same as a3: "<< (*a1 == *a3) <<endl;
+    cout<<"Is a2 same as a3: "<< (*a2 == *a3) <<endl;
     return 0;
 }
